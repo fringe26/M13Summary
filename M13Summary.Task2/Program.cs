3848b70350060fc104885d91074523a9992e4e70
Tural Aliyev <aliyevtural@itstep.org>
2022-08-20 21:27:09 +0400

    Add project files.

---
+++ M13Summary.Task2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.IO.MemoryMappedFiles;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+
+List<string> list = ReadFile();
+
+Console.WriteLine("10 Most Common words : \n");
+MostCommonWords(list).ToList().ForEach(Console.WriteLine);
+
+
+static List<string> ReadFile()
+{
+
+    List<string> list = new List<string>();
+
+    using (var mappedFile1 = MemoryMappedFile.CreateFromFile("Text1.txt"))
+    {
+        using (Stream mmStream = mappedFile1.CreateViewStream())
+        {
+            using (StreamReader sr = new StreamReader(mmStream, Encoding.UTF8))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+
+                    var noPunctuationText = new string(line.Where(c => !char.IsPunctuation(c)).ToArray());
+                    var lineWords = noPunctuationText.Split(' ').ToList<string>();
+                    lineWords.ForEach(list.Add);
+
+                }
+            }
+        }
+        //list.ForEach(Console.WriteLine);
+        
+    }
+    return list;
+}
+
+
+
+static IEnumerable<string> MostCommonWords(List<string> list)
+{
+    Dictionary<string,int> keyValuePairs = new Dictionary<string,int>();
+
+    foreach(string word in list)
+    {
+        if (!keyValuePairs.ContainsKey(word))
+        {
+            keyValuePairs[word] = 0;
+        }
+        else
+        {
+            keyValuePairs[word] = keyValuePairs[word] + 1;
+        }
+    }
+
+    keyValuePairs = keyValuePairs.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+
+
+    var words = keyValuePairs.Take(10).Select(x => x.Key);
+
+    return words;
+}
+
+
